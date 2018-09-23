d21ff327f1d8ea3bb1712cadaa8edbaca6ce3216
pytpriscila <pytpriscila@hotmail.com>
2018-09-23 12:06:35 -0300

    alter-to-run

--- code/application.py
+++ code/application.py
@@ -3,6 +3,8 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from flaskext.mysql import MySQL
+import constraint
 import ast
 
 from settings import APP_STATIC
