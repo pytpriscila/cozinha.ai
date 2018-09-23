da8223188049accc2b68743118b9619fbd09792d
pytpriscila <pytpriscila@hotmail.com>
2018-09-23 12:37:52 -0300

    Devmind commit in 2018-09-23 12:37:51

--- code/application.py
+++ code/application.py
@@ -31,7 +31,7 @@ def show_recipes():
     suggestions = cooking_recommendation.loc[(cooking_recommendation['serving_number']==int(serving_number)) 
     & (cooking_recommendation['time']==int(time)) & (cooking_recommendation['rating']==int(rating)), :]
     entries = (cooking_recommendation[:max_results])
-    error = false
+    error = ''
     return render_template('recipeRecommend.html', entries=entries, error=error)
 
 @application.route('/', methods=['GET', 'POST'])
