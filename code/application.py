270c04a6ac1ba5193fcaecc4af847f4dff3b8990
pytpriscila <pytpriscila@hotmail.com>
2018-09-23 11:48:23 -0300

    Retirada codigos inuteis

--- code/application.py
+++ code/application.py
@@ -3,8 +3,6 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-# flaskext.mysql import MySQL
-import constraint
 import ast
 
 from settings import APP_STATIC
@@ -22,39 +20,15 @@ usecols=['index','cuisine','id','rating','time', 'ingredient_amount', 'serving_n
 
 
 @application.route('/show')
-
 def show_recipes():
-    
     max_results=3
     serving_number  = request.args.get('serving_number')
     time = request.args.get('time')
     rating = request.args.get('rating')
- 
-    
     suggestions = cooking_recommendation.loc[(cooking_recommendation['serving_number']==serving_number) 
     & (cooking_recommendation['time']==time) & (cooking_recommendation['rating']==rating), :]
     
     print (cooking_recommendation[:max_results])
-
-    #return render_template('recipeRecommend.html', entries=entries, error=error)
-"""
-    try:
-        conn = mysql.connect()
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM recipe_info WHERE dbscan_label = %s;", [cur_flavor])
-        fetch_result = cur.fetchall()
-        entries = constraint.nutritional_constraints(fetch_result, cur_age, cur_weight, cur_height, cur_gender, 'Active')
-        print entries
-        conn.close()
-        error = None
-        return render_template('content.html', entries=entries, error=error)
-    except Exception as e:
-        print str(e)
-        entries = None
-        error = 'Database Connection Error!'
-        return render_template('content.html', entries=entries, error=error)
-"""
-
 
 @application.route('/', methods=['GET', 'POST'])
 def choose_flavor():
@@ -70,7 +44,6 @@ def choose_flavor():
         return render_template('index.html')
 
 
-application.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
